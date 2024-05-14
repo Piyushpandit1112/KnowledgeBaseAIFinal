8f3b1f2459bfb55b7f298ddaaac0dfa04c7515cc
Piyush <piyushtiwary4141@gmail.com>
2024-05-14 23:15:19 +0530

    Update ChatBotUI.py

--- ChatBotUI.py
+++ ChatBotUI.py
@@ -7,7 +7,7 @@ with st.sidebar:
     st.write("")  # Add some space at the top
     st.markdown("**History**")
 
-api_key=st.secrets["OPENAI_API_KEY"]
+api_key=st.secrets["auth_token"]
 
 client = OpenAI(api_key=api_key)
 
